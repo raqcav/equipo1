4139f67903dc3be7a6f8bf3cf7055f8f047fc9bb
unknown <ferran.vilanova@iesserpis.org>
2022-02-04 17:02:13 +0100

    commit1

---
+++ loops1.java
@@ -0,0 +1,14 @@
+
+public class loops1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        int i;
+        for(i=1;i<=10;i)
+        {
+        }
+        
+        bufferedReader.close();
+    }
+}
